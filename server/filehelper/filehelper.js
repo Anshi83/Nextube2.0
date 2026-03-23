8e7e601648f18bdf6b8a14064194fda7d2e50811
anshi83 <singhanshika5448@gmail.com>
2026-03-24 00:04:55 +0530

    check

--- server/filehelper/filehelper.js
+++ server/filehelper/filehelper.js
@@ -2,6 +2,11 @@ import { v2 as cloudinary } from "cloudinary";
 import multer from "multer";
 import * as cloudinaryStorage from "multer-storage-cloudinary";
 
+console.log("Cloudinary config check:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET ? "exists" : "MISSING"
+});
 const CloudinaryStorage =
   cloudinaryStorage.CloudinaryStorage ||
   cloudinaryStorage.default?.CloudinaryStorage ||
